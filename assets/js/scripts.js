5e9657ef5d04ed64abc9b6a087d66d578fc24bee
hada <hadathing@gmail.com>
2022-11-08 17:41:37 -0800

    Add keydown event for earthquake page.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,12 @@
-console.log('hello');
+window.addEventListener('keydown', event => {
+	switch (event.key) {
+		case '0':
+			location.href = './index.html';
+			break;
+		case '1':
+			document.querySelector('.video > video').play();
+			break;
+		default:
+			break;
+	}
+});
