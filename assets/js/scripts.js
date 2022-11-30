31504ee3ea6937382e32a78cf985695bd89e67bb
hada <hadathing@gmail.com>
2022-11-29 16:58:55 -0800

    Change map image, add increase avatar function.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -10,3 +10,13 @@ window.addEventListener('keydown', event => {
 			break;
 	}
 });
+
+const backgroundVideo = document.querySelector('.video');
+const leftDisasterInfo = document.querySelector('.left-box');
+const rightDisasterInfo = document.querySelector('.right-box');
+const bottomDisasterInfo = document.querySelector('.bottom-box');
+const avatarVideoWrapper = document.querySelector('.avatar');
+const avatarVideo = document.querySelector('.avatar > video');
+const qrInfo = document.querySelector('.qr-box');
+const qrAvatarVideoWrapper = document.querySelector('.qr-avatar');
+const qrAvatarVideo = document.querySelector('.qr-avatar > video');
